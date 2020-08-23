56885ea81466f38b343b2ae387d0b8f83abbeaca
GuoAi <guo__ai@hotmail.com>
2020-08-23 14:04:24 +0800

    Add "add" and "list" methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
  class Duke {
     static Scanner sc = new Scanner(System.in);
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void echo(String command) {
         System.out.println("    ____________________________________________________________");
@@ -25,6 +27,22 @@ import java.util.Scanner;
         System.out.println();
     }
 
+    public static void list() {
+        System.out.println("    ____________________________________________________________");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("     " + (i + 1) + ". " + tasks.get(i).getName());
+        }
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void add(String task) {
+        tasks.add(new Task(task));
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     added: " + task);
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -38,8 +56,10 @@ import java.util.Scanner;
             if (command.equals("bye")) {
                 exit();
                 return;
+            } else if (command.equals("list")) {
+                list();
             } else {
-                 echo(command);
+                add(command);
             }
         }
     }
